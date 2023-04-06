fb0ffd246b73c7c72520c781949570cc6cc3f1e4
Ghosty <ghosty@Ghostys-MacBook-Air.local>
2023-04-06 15:21:59 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+<img src="https://blacksla.sh/github/img/blackslash-logo.svg" alt="BlackSlash Creative Logo" width="400" />
+
+# Formik Login Form
+A validated login form built with React and Formik. 
+
+*This is an assignment I did through MIT, for my* **Professional Certificate in Coding: Full Stack Development with MERN.**
+
+## Installation & Usage
+Import Formik 
+<code>import {useFormik} from 'formik';</code>
+
+Copy code from App.js to use in your own app! 
+
+## Support
+If you need help with your React project, you can email me here: [help@blacksla.sh](mailto:help@blacksla.sh)
+
+## License
+The MIT license gives users express permission to reuse code for any purpose. 
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+// TODO: import useFormik from formik library
+import {useFormik} from 'formik';
+
+function App() {
+  // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    }, 
+    onSubmit: values => {
+      alert('Login Successful');
+    }, 
+    validate: values => {
+      let errors = {};
+      if (!values.email) {
+        errors.email = 'Field required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Username should be an email';
+      }
+      if (!values.password) errors.password = 'Field required';
+      return errors;
+    }
+  });
+
+  return (
+    <div class="login-form">
+      <form onSubmit={formik.handleSubmit}>
+        <label for="emailField">Email</label>
+        <input id="emailField" class="form-control" name="email" type="text" onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.email ? <div id="emailError" style={{color:'red'}}>{formik.errors.email}</div> : null}
+        <label for="pswField">Password</label>
+        <input id="pswField" class="form-control" name="password" type="text" onChange={formik.handleChange} value={formik.values.password}/>  
+        {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div> : null}
+        <br/>
+        <button id="submitBtn" class="btn btn-primary" type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
